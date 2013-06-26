4e04bd3c8de6bbcf73cd6c79a4e034f7257527e7
Michael Uman <Michael_Uman@sigmadesigns.com>
2013-06-26 15:59:34 -0700

    Added GPL license information to source files
    * Added GPL to source and header files.

--- magicls/main.cpp
+++ magicls/main.cpp
@@ -1,3 +1,27 @@
+/*
+    wxMagic - Magic file identification for wxWidgets
+    Copyright (C) 2013  Michael A. Uman
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ *  @file       main.cpp
+ *  @author :   Michael Uman
+ *  @date   :   June 26, 2013
+ */
+
 #include <stdio.h>
 #include <wx/wx.h>
 #include <wx/dir.h>
@@ -24,11 +48,14 @@ int main(int argc, const char* argv[])
 
     while (cont) {
         wxFileName  fname(argv[1], sFilename);
-        wxString sMimeType;
+        wxString sMimeType, sFileType;
 //        wxString sTmp = fname.GetFullPath();
 //        wxPrintf("sTmp %s\n", sTmp.c_str());
+        magic.GetFileType( fname.GetFullPath(), sFileType );
         magic.GetFileMimeType( fname.GetFullPath(), sMimeType );
-        wxPrintf("%-48.48s %s\n", sFilename.c_str(), sMimeType.c_str());
+        wxPrintf("%-40.40s %-32.32s %-40.40s\n", sFilename.c_str(), 
+                                              sMimeType.c_str(),
+                                              sFileType.c_str());
         cont = dir.GetNext( &sFilename );
     }
 
